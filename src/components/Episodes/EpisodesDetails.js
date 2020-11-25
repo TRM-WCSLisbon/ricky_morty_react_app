b36735b257a3f99354275ae2e86ccc9a2d59cc78
nemkanta09 <nemkanta@gmail.com>
2020-11-25 16:57:59 +0000

    episodedetail with style without character list

--- src/components/Episodes/EpisodesDetails.js
+++ src/components/Episodes/EpisodesDetails.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import API from "../API";
+import {
+  HeaderDetails,
+  Container,
+  CardGridDetails,
+  CardDetails,
+} from "./styles";
 import EpisodeImg from "./image.png";
 
 class EpisodesDetails extends React.Component {
@@ -31,20 +37,58 @@ class EpisodesDetails extends React.Component {
   };
 
   render() {
-    // console.log(this.state.episode);
+    const {
+      name,
+      air_date,
+      episode,
+      character,
+      url,
+      created,
+    } = this.state.episode;
     return (
       <div>
-        <img
-          src={EpisodeImg}
-          alt={this.state.episode && this.state.episode.name}
-        />
-        <p>Name: {this.state.episode && this.state.episode.name}</p>
-        {/* <p>Episode: {this.state.episode && this.state.episode.episode}</p>
-        <p>Created: {this.state.episode && this.state.episode.created}</p>
-        <p>URL: {this.state.episode && this.state.episode.url}</p>
-        <button>
-          <Link to="/episodes">Return to Episodes</Link>
-        </button> */}
+        <HeaderDetails>
+          <header>
+            <h1>{this.state.episode && name}</h1>
+          </header>
+        </HeaderDetails>
+        <Container>
+          <CardGridDetails>
+            <CardDetails>
+              <div>
+                <div className="column-left">
+                  <img
+                    src={this.state.episode && EpisodeImg}
+                    alt={this.state.episode && name}
+                  />
+                </div>
+                <div className="column-right">
+                  {/* <h1>Name: {this.state.episode.name}</h1> */}
+                  <ul>
+                    <li>
+                      <h3>Episode:</h3>{" "}
+                      <span>{this.state.episode && episode}</span>
+                    </li>
+                    <li>
+                      <h3>Air Date:</h3>{" "}
+                      <span>{this.state.episode && air_date}</span>
+                    </li>
+                    {/* <li>
+                      <h3>URL:</h3> <span>{this.state.episode && url}</span>
+                    </li> */}
+                    <li>
+                      <h3>Created Date:</h3>{" "}
+                      <span>{this.state.episode && created}</span>
+                    </li>
+                  </ul>
+                  <button>
+                    <Link to="/episodes">Return to Episodes</Link>
+                  </button>{" "}
+                </div>
+              </div>
+            </CardDetails>
+          </CardGridDetails>
+        </Container>
       </div>
     );
   }
