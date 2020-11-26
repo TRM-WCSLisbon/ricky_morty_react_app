0bc664751d3ca5f1c16263e80c1e0efc22791d87
nemkanta09 <nemkanta@gmail.com>
2020-11-26 18:06:20 +0000

    season images added and working

--- src/components/Episodes/EpisodesDetails.js
+++ src/components/Episodes/EpisodesDetails.js
@@ -1,144 +1,3 @@
-// import React from "react";
-// // import { Link } from "react-router-dom";
-// import { NavLink as Link } from "react-router-dom";
-// // import ArrowBack from "@material-ui/icons/ArrowBack";
-// import API from "../API";
-// import {
-//   HeaderDetails,
-//   Container,
-//   CardGridDetails,
-//   CardDetails,
-//   // Characters,
-// } from "./styles";
-// import EpisodeImg from "./image.png";
-// import Characters from "../Characters/Characters";
-
-// class EpisodesDetails extends React.Component {
-//   state = {
-//     episode: {},
-//     character: [],
-//   };
-
-//   componentDidMount() {
-//     this.getEpisodes();
-//     this.getCharacters();
-//     console.log("hello");
-//   }
-
-//   componentDidUpdate(prevProps) {
-//     console.log(prevProps);
-//     if (prevProps.match.params.id !== this.props.match.params.id) {
-//       this.getEpisodes();
-//     }
-//   }
-
-//   getEpisodes = () => {
-//     const episodeId = Number(this.props.match.params.id);
-//     //console.log(this.props);
-//     // console.log(episodeId);
-//     API.get(`/episode/${episodeId}`).then(
-//       (response) =>
-//         //console.log(response.data)
-//         this.setState({ episode: response.data })
-//       //console.log(this.state.episode)
-//     );
-//   };
-
-//   getCharacters = () => {
-//     const currentEpisodeId = Number(this.props.match.params.id);
-//     API.get(`/episode/${currentEpisodeId}`).then(
-//       (response) => (
-//         // console.log(response)
-//         console.log(typeof response.data.characters, response.data.characters),
-//         response.data.characters
-//           .map(
-//             (chr) =>
-//               // console.log(typeof chr, chr)))
-//               chr.split("/character/")[1]
-//           )
-//           .map((chr) =>
-//             // console.log(chr);
-//             // <li key={chr}>{chr}</li>;
-
-//             this.setState({ character: [...this.state.character, chr] })
-//           )
-//       )
-//     );
-//   };
-
-//   render() {
-//     const { name, air_date, episode, created } = this.state.episode;
-
-//     // const { characters, episode } = this.state;
-
-//     return (
-//       <div>
-//         <HeaderDetails>
-//           <header>
-//             <div className="IconBack">
-//               <Link to="/episodes">
-//                 <ArrowBack style={{ fontSize: 60 }} />
-//               </Link>
-//             </div>
-//             <h1>{this.state.episode && name}</h1>
-//           </header>
-//         </HeaderDetails>
-//         <Container>
-//           <CardGridDetails>
-//             <CardDetails>
-//               <div>
-//                 <div className="column-left">
-//                   <img
-//                     src={this.state.episode && EpisodeImg}
-//                     alt={this.state.episode && name}
-//                   />
-//                 </div>
-//                 <div className="column-right">
-//                   {/* <h1>Name: {this.state.episode.name}</h1> */}
-//                   <ul>
-//                     <li>
-//                       <h3>Episode:</h3>{" "}
-//                       <span>{this.state.episode && episode}</span>
-//                     </li>
-//                     <li>
-//                       <h3>Air Date:</h3>{" "}
-//                       <span>{this.state.episode && air_date}</span>
-//                     </li>
-//                     {/* <li>
-//                       <h3>URL:</h3> <span>{this.state.episode && url}</span>
-//                     </li> */}
-//                     <li>
-//                       <h3>Created Date:</h3>{" "}
-//                       <span>{this.state.episode && created}</span>
-//                     </li>
-//                     <li>
-//                       <h3>Characters: </h3>{" "}
-//                       <span>
-//                         {this.state.character &&
-//                           this.state.character.map((char) => (
-//                             <li>
-//                               <Link to={`/episodes/${char}`}>{char}</Link>
-//                             </li>
-//                           ))}
-//                       </span>
-//                     </li>
-//                   </ul>
-//                   {/* <button>
-//                     <Link to="/episodes">Return to Episodes</Link>
-//                   </button> */}
-//                 </div>
-//               </div>
-//             </CardDetails>
-//           </CardGridDetails>
-//         </Container>
-//       </div>
-//     );
-//   }
-// }
-
-// export default EpisodesDetails;
-
-/* eslint-disable react/state-in-constructor */
 import React from "react";
 import API from "../API";
 import { Link } from "react-router-dom";
@@ -148,7 +7,11 @@ import {
   CardGridDetails,
   CardDetails,
 } from "./styles";
-import EpisodeImg from "./image.png";
+// import EpisodeImg from "./image.png";
+import Season1 from "./season1.jpg";
+import Season2 from "./season2.jpg";
+import Season3 from "./season3.jpg";
+import Season4 from "./season4.jpg";
 
 class EpisodesDetails extends React.Component {
   state = {
@@ -157,7 +20,7 @@ class EpisodesDetails extends React.Component {
   };
 
   componentDidMount() {
-    this.getEpisodes();
+    this.getEpisode();
     this.getCharacters();
     // console.log(this.state.location);
   }
@@ -165,14 +28,14 @@ class EpisodesDetails extends React.Component {
   componentDidUpdate(prevProps) {
     // console.log(prevProps);
     if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.getEpisodes();
+      this.getEpisode();
     }
   }
 
-  getEpisodes = () => {
+  getEpisode = () => {
     const episodeId = Number(this.props.match.params.id);
     // console.log(this.props);
-    // console.log(episodeId);
+    console.log(episodeId);
     API.get(`/episode/${episodeId}`).then(
       (response) => this.setState({ episode: response.data })
       // console.log(this.state.episode)
@@ -181,38 +44,57 @@ class EpisodesDetails extends React.Component {
 
   getCharacters = () => {
     const episodeId = Number(this.props.match.params.id);
-    API.get(`/episode/${episodeId}`).then(
-      (response) => (
-        console.log(typeof response.data.characters, response.data.characters),
-        response.data.characters
-          .map((char) => char.split("character/")[1])
-          .map((char) =>
-            this.setState({ character: [...this.state.character, char] })
-          )
-      )
+    API.get(`/episode/${episodeId}`).then((response) =>
+      // console.log(typeof response.data.characters, response.data.characters)
+      response.data.characters
+        .map((char) => char.split("character/")[1])
+        .map((char) =>
+          this.setState({ character: [...this.state.character, char] })
+        )
     );
+  };
+
+  returnEpisodeImage = (season) => {
+    console.log(season);
+    switch (true) {
+      case season.includes("S01"):
+        return Season1;
+        break;
+      case season.includes("S02"):
+        return Season2;
+        break;
+      case season.includes("S03"):
+        return Season3;
+        break;
+      default:
+        return Season4;
+    }
   };
 
   render() {
     return (
       <div>
         <HeaderDetails>
-          <button>
-            <Link to={`/episodes?page=1`}>Go back</Link>
-          </button>
           <header>
             <h1>{this.state.episode && this.state.episode.name}</h1>
           </header>
         </HeaderDetails>
+
         <Container>
           <CardGridDetails>
             <CardDetails>
               <div>
                 <div className="column-left">
                   <img
-                    src={this.state.episode && EpisodeImg}
-                    alt={this.state.episode && name}
+                    src={
+                      this.state.episode.episode &&
+                      this.returnEpisodeImage(this.state.episode.episode)
+                    }
+                    alt={this.state.episode && this.state.episode.name}
                   />
+                  <button>
+                    <Link to={`/episodes?page=1`}>Go to Episode List</Link>
+                  </button>
                 </div>
                 <div className="column-right">
                   {/* <h1>Name: {this.state.episode.name}</h1> */}
